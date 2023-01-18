4fc3577efe814ab472608e14f5f94f862e87f49b
Eric Elsner <ericelsnercodes@gmail.com>
2023-01-18 14:14:52 -0500

    login/logout/signup done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,7 +22,7 @@ const router = express.Router()
 router.post('/signup', async (req, res) => {
     // this route will take a req.body and use that data to create a user
     const newUser = req.body
-    console.log('this is req.body', req.body)
+    // console.log('this is req.body', req.body)
     // we'll need to encrypt their password
     // this is where bcrypt comes into play
     // for the sake of bcrypt, we're going to use async / await
@@ -30,13 +30,79 @@ router.post('/signup', async (req, res) => {
         newUser.password,
         await bcrypt.genSalt(10)
     )
-    res.json({ newUser: newUser })
     // then create the user
-    // if we're successful, send a 201 status
-    // if there is an error, handle the error
+    User.create(newUser)
+        // if we're successful, send a 201 status
+        .then(user => {
+            // console.log('new user created \n', user)
+            res.status(201).json({ username: user.username})
+        })
+        // if there is an error, handle the error
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
 })
 
+// POST -> /users/login
+// this route creates a new session in our db and our browser
 
+router.post('/login', async (req, res) => {
+    // destructure the username and pw from our request body
+    const { username, password } = req.body
+
+    // search the db for a  user with a specific username
+    User.findOne({ username })
+        .then(async (user) => {
+            // we check if that user exist, 
+            if (user) {
+                // if they do we compare the pw's using bcrypt
+                // bcrypt.compare -> evaluates to a truthy or falsey value
+                // we'll save that result to a variable
+                // password -> req.body
+                // user.password -> saved in database
+                const result = await bcrypt.compare(password, user.password)
+
+                
+                if (result) {
+                    // if the pw's match, place the user's info in the session
+                    // this is where we use the session object that lives in our request object
+                    req.session.username = username
+                    req.session.loggedIn = true
+                    req.session.userId = user.id
+
+                    // console.log('this is req.session \n', req.session)
+
+                    // we'll send a 201 response
+                    res.status(201).json({ username: user.username })
+                } else {
+                    // if the pw's don't match, send the user a message
+                    res.json({ error: 'username or password is incorrect'})
+                }
+            } else {
+                // if a user does not exist, we respond with a message saying so
+                res.json({ error: 'user does not exist' })
+            }
+             
+        })
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+
+})
+
+// DELETE -> /users/logout
+// this route destroys a session in our db and the browser
+router.delete('/logout', (req, res) => {
+    // destroy session and send an appropriate response
+    req.session.destroy(err => {
+        console.log('this is req.session upon logout \n', req.session)
+        console.log('error on logout? \n', err)
+        //eventually we will rediect users here, after view layer
+        res.sendStatus(204)
+    })
+})
 
 // ***********
 // Export Router
--- utils/middleware.js
+++ utils/middleware.js
@@ -4,6 +4,9 @@
 
 const express = require('express') // import the express framework
 const morgan = require('morgan') // import the morgan request logger
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+require('dotenv').config()
 
 // ***********
 // Middleware Function
@@ -23,6 +26,22 @@ const middleware = (app) => {
     app.use(express.urlencoded({ extended: true })) // this parses urlEncoded reuest bodies, useful for POST and PUT requests
     app.use(express.static('public')) // this serves static files from the 'public' folder
     app.use(express.json()) // this parses incoming request payloads with JSON
+    // setup and utilize a session function, and we pass that function a config argument to configure our session. This argument will tell express-session how to create and store our session.
+    //the config object needs several jeys to work(see express-session docs)
+    // the keys are:
+    // secret -  a super top secret code that allows for the creation of a session; kinda like auth, it allows our app to create with connectMongo
+    // store - tells connectMongo where to save the session(our db)
+    // then two options: saveUninitialized(true) and resave(false)
+    app.use(
+        session({
+            secret: process.env.SECRET,
+            store: MongoStore.create({
+                mongoUrl: process.env.DATABASE_URL,
+                saveUninitialized: true,
+                resave: false
+            })
+        })
+    )
 }
 
 // ***********
